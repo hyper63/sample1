fee6a06434e5126ba2b03666e90040dc1e15dbf7
Antonio Dinkins <afdinki@gmail.com>
2022-01-13 19:52:58 +0000

    Added front end toolchain dependencies for React app

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import reactDom from "react-dom";
+import App from "./App";
+
+const elem = document.getElementById("app");
+reactDom.render(<App/>, elem);
